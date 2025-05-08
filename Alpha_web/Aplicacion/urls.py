2728b4fd2e9d8275dc1ba45ea620d0275ba8d055
ANDRES FLORIAN <DUVANFLORIANSALAZAR@GMAIL.COM>
2025-05-07 21:26:12 -0500

    fix: :bug: Actualizar caja fuerte

--- Alpha_web/Aplicacion/urls.py
+++ Alpha_web/Aplicacion/urls.py
@@ -1,3 +1,4 @@
+# urls.py (corregido)
 from django.urls import path
 from Aplicacion import views
 from django.conf import settings
@@ -8,12 +9,14 @@ urlpatterns = [
     path('registro/', views.Registro, name='registro'),
     path('inicio-sesion/', views.Inicio_Sesion, name='Inicio_Sesion'),
     path('planes/', views.Planes, name='Planes'),
-    path('caja-fuerte/', views.caja_fuerte, name='CajaFuerte'),
+    path('cajaFuerte/', views.caja_fuerte, name='CajaFuerte'),
     path('contrasenas/', views.Contrasenas, name='Contrasenas'),
     path('cerrar-sesion/', views.cerrar_sesion, name='Logout'),
-    path('caja-fuerte/', views.caja_fuerte, name='caja_fuerte'),
-    path('caja-fuerte/subir/', views.subir_documento, name='subir_documento'),
-    path('caja-fuerte/ver/<int:documento_id>/', views.ver_documento, name='ver_documento'),
-    path('caja-fuerte/eliminar/<int:documento_id>/', views.eliminar_documento, name='eliminar_documento'),
+    
+    # Documentos
+    path('subir-documento/', views.subir_documento, name='SubirDocumento'),
+    path('ver-documento/<int:documento_id>/', views.ver_documento, name='VerDocumento'),
+    path('eliminar-documento/<int:documento_id>/', views.eliminar_documento, name='EliminarDocumento'),
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
